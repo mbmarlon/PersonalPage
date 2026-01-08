9c6fbf1f3376de8e3ab10d6cde32de56e672d89f
mbmarlon <89369332+mbmarlon@users.noreply.github.com>
2026-01-08 13:11:05 +0000

    Creando el admin Se agrega la carpeta Admin, con Admin y UploadPost

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./styles.css";
 import Header from "./Component/Header.jsx";
 import Nav from "./Component/Nav.jsx";
 import Footer from "./Component/Footer.jsx";
+import Admin from "./Admin/Admin.jsx";
 
 export default function App() {
   return (
@@ -9,6 +12,11 @@ export default function App() {
       <Header />
       <Nav />
       <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
